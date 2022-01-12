5b958aa21512971eb38284fd0429a09f14f3ab15
Nikolaos Vastardis <Nikolaos.Vastardis@gla-rad.org>
2022-01-12 15:47:10 +0000

    Removing the old controller test annotations

--- src/test/java/org/grad/eNav/apiGateway/controllers/HTMLViewerControllerTest.java
+++ src/test/java/org/grad/eNav/apiGateway/controllers/HTMLViewerControllerTest.java
@@ -17,16 +17,13 @@
 package org.grad.eNav.apiGateway.controllers;
 
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
 @ActiveProfiles("test")
-@ExtendWith(SpringExtension.class)
 @WebFluxTest(HTMLViewerController.class)
 class HTMLViewerControllerTest {
 
