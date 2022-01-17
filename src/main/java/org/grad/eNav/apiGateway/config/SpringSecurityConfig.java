8dc930c0809ec47d79811816b49d8bb71c9e5e94
Nikolaos Vastardis <n.vastardis@gmail.com>
2022-01-17 14:13:04 +0000

    Updating the security configuration to authorise api requests

--- src/main/java/org/grad/eNav/apiGateway/config/SpringSecurityConfig.java
+++ src/main/java/org/grad/eNav/apiGateway/config/SpringSecurityConfig.java
@@ -24,7 +24,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.method.configuration.EnableReactiveMethodSecurity;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.oauth2.client.oidc.web.server.logout.OidcClientInitiatedServerLogoutSuccessHandler;
+import org.springframework.security.oauth2.client.registration.ReactiveClientRegistrationRepository;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.security.web.server.header.XFrameOptionsServerHttpHeadersWriter;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -51,7 +54,14 @@ class SpringSecurityConfig {
      * Finally, all other exchanges need to be authenticated.
      */
     @Bean
-    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
+    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http,
+                                                            ReactiveClientRegistrationRepository clientRegistrationRepository) {
+        // Authenticate through configured OpenID Provider
+        http.oauth2Login();
+        // Also, logout at the OpenID Connect provider
+        http.logout(logout -> logout.logoutSuccessHandler(
+                new OidcClientInitiatedServerLogoutSuccessHandler(clientRegistrationRepository)));
+        // Require authentication for all requests
         http.authorizeExchange(exchanges ->
                     exchanges.matchers(EndpointRequest.to("health", "info")).permitAll()
                             .and().authorizeExchange()
@@ -63,7 +73,11 @@ class SpringSecurityConfig {
                             .and().authorizeExchange()
                             .anyExchange().authenticated()
                 )
-                .oauth2Login(withDefaults());
+                .oauth2Login(withDefaults())
+                .oauth2ResourceServer().jwt();
+        // Allow showing /home within a frame
+        http.headers().frameOptions().mode(XFrameOptionsServerHttpHeadersWriter.Mode.SAMEORIGIN);
+        // Disable the CSRF
         http.csrf().disable();
         return http.build();
     }
