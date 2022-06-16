ee7d9c9fc9b698723aecd555419168998884b353
Nikolaos Vastardis <n.vastardis@gmail.com>
2022-06-16 12:38:12 +0100

    Adding a programmatic CORS configuration

---
+++ src/main/java/org/grad/eNav/apiGateway/config/ApiCorsConfiguration.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright (c) 2021 GLA Research and Development Directorate
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.grad.eNav.apiGateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+
+@Configuration
+public class ApiCorsConfiguration extends CorsConfiguration {
+
+    @Bean
+    public CorsWebFilter corsFilter() {
+        org.springframework.web.cors.CorsConfiguration corsConfiguration = new org.springframework.web.cors.CorsConfiguration();
+        corsConfiguration.setAllowCredentials(true);
+        corsConfiguration.addAllowedOrigin("*");
+        corsConfiguration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD"));
+        corsConfiguration.addAllowedHeader("origin");
+        corsConfiguration.addAllowedHeader("content-type");
+        corsConfiguration.addAllowedHeader("accept");
+        corsConfiguration.addAllowedHeader("authorization");
+        corsConfiguration.addAllowedHeader("cookie");
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfiguration);
+        return new CorsWebFilter(source);
+    }
+
+}
