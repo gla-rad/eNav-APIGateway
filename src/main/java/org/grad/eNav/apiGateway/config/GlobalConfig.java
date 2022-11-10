5ceefad2d1cbc72a23594c7bb1894cf15b2fbaf7
Nikolaos Vastardis <n.vastardis@gmail.com>
2022-11-10 17:17:22 +0000

    Enabling the http trace actuator

---
+++ src/main/java/org/grad/eNav/apiGateway/config/GlobalConfig.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright (c) 2021 Maritime Connectivity Platform Consortium
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.grad.eNav.apiGateway.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.actuate.trace.http.HttpTraceRepository;
+import org.springframework.boot.actuate.trace.http.InMemoryHttpTraceRepository;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * The Global Configuration.
+ *
+ * A class to define the global configuration for the application.
+ *
+ * @author Nikolaos Vastardis (email: Nikolaos.Vastardis@gla-rad.org)
+ */
+@Configuration
+@Slf4j
+public class GlobalConfig {
+
+    /**
+     * <p>
+     * Add an HTTP trace repository in memory to be used for the respective
+     * actuator.
+     * </p>
+     * <p>
+     * The functionality has been removed by default in Spring Boot 2.2.0. For
+     * more info see:
+     * </p>
+     * <a href="https://github.com/spring-projects/spring-boot/wiki/Spring-Boot-2.2.0-M3-Release-Notes#actuator-http-trace-and-auditing-are-disabled-by-default">...</a>
+     *
+     * @return the in memory HTTP trance repository
+     */
+    @ConditionalOnProperty(value = "management.trace.http.enabled", havingValue = "true")
+    @Bean
+    public HttpTraceRepository httpTraceRepository() {
+        return new InMemoryHttpTraceRepository();
+    }
+
+}
