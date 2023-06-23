c791e93163b750e27e91508cf993d9ba8d8d94cd
Nikolaos Vastardis <n.vastardis@gmail.com>
2023-06-23 14:29:44 +0100

    Replacing the web-socket with the netty configuration

---
+++ src/main/java/org/grad/eNav/apiGateway/config/NettyConfiguration.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright (c) 2021 GLA Research and Development Directorate
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.grad.eNav.apiGateway.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.embedded.netty.NettyReactiveWebServerFactory;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * The Global Configuration.
+ *
+ * A class to customize the netty configuration and allow larger web-socket
+ * content sizes.
+ *
+ * @author Nikolaos Vastardis (email: Nikolaos.Vastardis@gla-rad.org)
+ */
+@Configuration
+public class NettyConfiguration implements WebServerFactoryCustomizer<NettyReactiveWebServerFactory> {
+
+    @Value("${server.max-initial-line-length:65536}")
+    private int maxInitialLingLength;
+    @Value("${server.max-http-header-size:65536}")
+    private int maxHttpHeaderSize;
+
+    public void customize(NettyReactiveWebServerFactory container) {
+        container.addServerCustomizers(
+                httpServer -> httpServer.httpRequestDecoder(
+                        httpRequestDecoderSpec -> {
+                            httpRequestDecoderSpec.maxHeaderSize(maxHttpHeaderSize);
+                            httpRequestDecoderSpec.maxInitialLineLength(maxInitialLingLength);
+                            return httpRequestDecoderSpec;
+                        }
+                )
+        );
+    }
+}
