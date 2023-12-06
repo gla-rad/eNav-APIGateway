8553421a4ab70f06ec78915d06317596873d993d
Nikolaos Vastardis <n.vastardis@gmail.com>
2023-12-06 16:05:24 +0000

    Adding back the auth resolver for testing

--- src/main/java/org/grad/eNav/apiGateway/config/SpringSecurityConfig.java
+++ src/main/java/org/grad/eNav/apiGateway/config/SpringSecurityConfig.java
@@ -38,9 +38,12 @@ import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.core.authority.mapping.GrantedAuthoritiesMapper;
 import org.springframework.security.core.session.SessionRegistryImpl;
 import org.springframework.security.oauth2.client.registration.ReactiveClientRegistrationRepository;
+import org.springframework.security.oauth2.client.web.server.DefaultServerOAuth2AuthorizationRequestResolver;
+import org.springframework.security.oauth2.client.web.server.ServerOAuth2AuthorizationRequestResolver;
 import org.springframework.security.web.authentication.session.RegisterSessionAuthenticationStrategy;
 import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.security.web.server.util.matcher.PathPatternParserServerWebExchangeMatcher;
 import org.springframework.web.client.RestTemplate;
 
 /**
@@ -147,8 +150,8 @@ class SpringSecurityConfig {
                                                             ReactiveClientRegistrationRepository clientRegistrationRepository,
                                                             RestTemplate restTemplate) {
         // Authenticate through configured OpenID Provide
-        http.oauth2Login(oauth2 ->
-                {}
+        http.oauth2Login(oauth2 -> oauth2
+                .authorizationRequestResolver(this.authorizationRequestResolver(clientRegistrationRepository))
 //                .authenticationMatcher(new PathPatternParserServerWebExchangeMatcher("{baseUrl}/login/oauth2/code/{registrationId}"))
         );
         // Also, logout at the OpenID Connect provider
@@ -191,4 +194,10 @@ class SpringSecurityConfig {
         return http.build();
     }
 
+    private ServerOAuth2AuthorizationRequestResolver authorizationRequestResolver(ReactiveClientRegistrationRepository clientRegistrationRepository) {
+        return new DefaultServerOAuth2AuthorizationRequestResolver(
+                clientRegistrationRepository,
+                new PathPatternParserServerWebExchangeMatcher("/enav/oauth2/authorization/keycloak")
+        );
+    }
 }
