fbf9e9dacfb610c2cbb34d22ace18745393ceb4d
Nikolaos Vastardis <n.vastardis@gmail.com>
2023-12-08 11:57:14 +0000

    Adding comments and cleaning up the StripContextAndPrefixGatewayFilterFactory

--- src/main/java/org/grad/eNav/apiGateway/components/StripContextAndPrefixGatewayFilterFactory.java
+++ src/main/java/org/grad/eNav/apiGateway/components/StripContextAndPrefixGatewayFilterFactory.java
@@ -29,23 +29,68 @@ import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
 import java.util.Arrays;
-import java.util.List;
 import java.util.stream.Collectors;
 
+/**
+ * The StripContextAndPrefixGatewayFilterFactory Class.
+ *
+ * When hosting the API-Gateway in a domain sub-path we need to route all
+ * requests to the microservices appropriately. However, the forwarding strategy
+ * that is able to correctly pick up the base-path (i.e. framework strategy)
+ * does not correctly route the discovered services calls, by not removing the
+ * base paths.
+ * <p/>
+ * This is related to a known issue in the spring cloud gateway and a workaround
+ * was suggested
+ * <a href="https://github.com/spring-cloud/spring-cloud-gateway/issues/1759">
+ *     here
+ * </a>.
+ * </p>
+ * This filter factor follows this work around to allow the correct service
+ * routing through the service configuration as follows:
+ * <p>
+ *  # Adding Route for the "/enav" sub-path support
+ *  spring.cloud.gateway.routes[0].id=service-discovery-route-msg-broker
+ *  spring.cloud.gateway.routes[0].uri=lb://msg-broker
+ *  spring.cloud.gateway.routes[0].predicates[0]=Path=/enav/msg-broker/**
+ *  spring.cloud.gateway.routes[0].filters[0]=StripContextAndPrefix=2
+ *  spring.cloud.gateway.routes[1].id=service-discovery-route-vdes-ctrl
+ *  spring.cloud.gateway.routes[1].uri=lb://vdes-ctrl
+ *  spring.cloud.gateway.routes[1].predicates[0]=Path=/enav/vdes-ctrl/**
+ *  spring.cloud.gateway.routes[1].filters[0]=StripContextAndPrefix=2
+ *  spring.cloud.gateway.routes[2].id=service-discovery-route-aton-service
+ *  spring.cloud.gateway.routes[2].uri=lb://aton-service
+ *  spring.cloud.gateway.routes[2].predicates[0]=Path=/enav/aton-service/**
+ *  spring.cloud.gateway.routes[2].filters[0]=StripContextAndPrefix=2
+ *  spring.cloud.gateway.routes[3].id=service-discovery-route-ckeeper
+ *  spring.cloud.gateway.routes[3].uri=lb://ckeeper
+ *  spring.cloud.gateway.routes[3].predicates[0]=Path=/enav/ckeeper/**
+ *  spring.cloud.gateway.routes[3].filters[0]=StripContextAndPrefix=2
+ * </p>
+ *
+ * @author Nikolaos Vastardis (email: Nikolaos.Vastardis@gla-rad.org)
+ */
 @Component
 public class StripContextAndPrefixGatewayFilterFactory extends AbstractGatewayFilterFactory<StripPrefixGatewayFilterFactory.Config> {
 
-
     public static final String PARTS_KEY = "parts";
 
+    /**
+     * The Class Constructor.
+     */
     public StripContextAndPrefixGatewayFilterFactory() {
         super(StripPrefixGatewayFilterFactory.Config.class);
     }
 
-    public List<String> shortcutFieldOrder() {
-        return Arrays.asList("parts");
-    }
-
+    /**
+     * Applies the filter's operation. In this the incoming web-exchange will
+     * be stripped of the first N parts. The context path for the call will also
+     * get stripped in a similar manner so that the annoying error does not
+     * occur.
+     *
+     * @param config the gateway filter factory configuration
+     * @return the initialised gateway filter
+     */
     public GatewayFilter apply(final StripPrefixGatewayFilterFactory.Config config) {
         return new GatewayFilter() {
             public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
@@ -84,21 +129,6 @@ public class StripContextAndPrefixGatewayFilterFactory extends AbstractGatewayFi
                 return GatewayToStringStyler.filterToStringCreator(StripContextAndPrefixGatewayFilterFactory.this).append("parts", config.getParts()).toString();
             }
         };
-    }
-
-    public static class Config {
-        private int parts = 1;
-
-        public Config() {
-        }
-
-        public int getParts() {
-            return this.parts;
-        }
-
-        public void setParts(int parts) {
-            this.parts = parts;
-        }
     }
 
 }
